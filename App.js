5bd3c77d3f75b94a5545af0a6038bd38073c9da9
김재민 <jameen@gimjaemins-MacBook-Pro.local>
2020-11-09 22:04:00 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,77 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import { v4 as uuidv4 } from 'uuid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const App = () => {
+
+  useEffect(() => {
+    readData()
+  }, [])
+
+  const [trees, setTrees] = useState({});
+  const [name, setName] = useState('');
+
+  const onChangeText = name => setName(name);
+
+  const onFinishEditing = () => {
+
+    const ID = uuidv4();
+
+    const newTreeObj = {
+      [ID]: {
+        id: id,
+        name: name,
+      }
+    }
+
+    setName('');
+
+    const newState = {
+      ...trees,
+      ...newTreeObj
+    }
+
+    setTrees({
+      ...trees,
+      ...newTreeObj
+    })
+
+    AsyncStorage.setItem("trees", JSON.stringify(newState));
+  }
+
+  
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+});
+
+export default App;
