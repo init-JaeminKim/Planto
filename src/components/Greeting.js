d3553485fa88f685ec6adeeb82b23eaae5152423
김재민 <jkim37@g.emporia.edu>
2020-11-15 19:43:50 -0600

    Add Fonts

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,18 +5,23 @@ import {
     StyleSheet,
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 const Greeting = () => {
 
     return (
         <View style={styles.GTContainer}>
             <Text>Hello</Text>
+            <Text>
+                <Icon name="rocket" size={30} color="#900" />
+            </Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     GTContainer: {
-        marginTop: 100, 
+        marginTop: 100,
         alignItems: 'center'
     }
 })
